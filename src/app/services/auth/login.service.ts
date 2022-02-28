9ef6df445913286bf3999c4729313c67c89f4efa
JUGAL KISHOR SINGH <jks2127@gmail.com>
2022-02-28 14:41:15 +0530

    firebase email-password authentication complete

--- src/app/services/auth/login.service.ts
+++ src/app/services/auth/login.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
-
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private route: Router, private menuCtrl: MenuController) { }
-  check(credential: any) {
-    if(credential.username==='jks' && credential.password==='123'){
-      this.route.navigate(['/home-page']);
-      this.menuCtrl.enable(true);
-      localStorage.setItem('Token', 'lksdgdhfjjsdakl43hj2khr3458p42hrwehafl84r8');
-    }else {
-      this.route.navigate(['/']);
+  constructor(private auth:Auth , private route: Router, private menuCtrl: MenuController) { }
+  
+  async login ({email, password}) {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (error) {
+      console.error("Error while signing in................");
     }
+  }
+
+  async register ({email, password}) {
+    try {
+      const user = await createUserWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (error) {
+      console.error("Error while registering the user.......");      
+    }
+  }
+
+  logout() {
+    return signOut(this.auth);
   }
 }
